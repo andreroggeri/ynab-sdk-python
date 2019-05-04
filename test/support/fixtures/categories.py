cf513a0614c9e648c9335e0e2b10433ecb2e7511
André Roggeri Campos <a.roggeri.c@gmail.com>
2019-05-03 21:38:58 -0300

    test: Add tests for CategoriesApi

---
+++ test/support/fixtures/categories.py
@@ -0,0 +1,54 @@
+VALID_CATEGORIES = {
+    "data": {
+        "category_groups": [
+            {
+                "id": "string",
+                "name": "string",
+                "hidden": True,
+                "deleted": True,
+                "categories": [
+                    {
+                        "id": "string",
+                        "category_group_id": "string",
+                        "name": "string",
+                        "hidden": True,
+                        "original_category_group_id": "string",
+                        "note": "string",
+                        "budgeted": 0,
+                        "activity": 0,
+                        "balance": 0,
+                        "goal_type": "TB",
+                        "goal_creation_month": "string",
+                        "goal_target": 0,
+                        "goal_target_month": "string",
+                        "goal_percentage_complete": 0,
+                        "deleted": True
+                    }
+                ]
+            }
+        ],
+        "server_knowledge": 0
+    }
+}
+
+VALID_CATEGORY = {
+    "data": {
+        "category": {
+            "id": "string",
+            "category_group_id": "string",
+            "name": "string",
+            "hidden": True,
+            "original_category_group_id": "string",
+            "note": "string",
+            "budgeted": 0,
+            "activity": 0,
+            "balance": 0,
+            "goal_type": "TB",
+            "goal_creation_month": "string",
+            "goal_target": 0,
+            "goal_target_month": "string",
+            "goal_percentage_complete": 0,
+            "deleted": True
+        }
+    }
+}
