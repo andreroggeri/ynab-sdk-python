ad0863d989d5470cc993ece9fc437cf665ef5b21
David Lerner <dmlerner@gmail.com>
2019-11-12 22:31:44 -0800

    added update transaction api

--- ynab_sdk/api/transactions.py
+++ ynab_sdk/api/transactions.py
@@ -3,6 +3,7 @@ from typing import List
 
 from ynab_sdk.api.models.requests.transaction import TransactionRequest
 from ynab_sdk.api.models.responses.transactions import TransactionsResponse
+from ynab_sdk.api.models.responses.budget_detail import Transaction
 from ynab_sdk.utils.clients.base_client import BaseClient
 
 
@@ -20,3 +21,11 @@ class TransactionsApi:
             'transactions': [dataclasses.asdict(t) for t in transactions]
         }
         return self.client.post(f'/budgets/{budget_id}/transactions', payload)
+
+    def update_transaction(self, budget_id: str, transaction: Transaction) -> TransactionsResponse:
+        payload = {
+                'transaction': dataclasses.asdict(transaction)
+        }
+        transaction_id = transaction.id
+        response = self.client.put(f'/budgets/{budget_id}/transactions/{transaction_id}', payload)
+        return response
