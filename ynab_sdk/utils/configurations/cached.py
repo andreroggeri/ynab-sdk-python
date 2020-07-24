362ece4124bf4d557c0963ff7cee77406726862e
Tommaso-n <68448080+Tommaso-n@users.noreply.github.com>
2020-07-25 01:48:25 +0200

    Update cached.py

--- ynab_sdk/utils/configurations/cached.py
+++ ynab_sdk/utils/configurations/cached.py
@@ -1,11 +1,20 @@
 from ynab_sdk.utils.configurations.default import DefaultConfig
+from typing import Union
 
 
 class CachedConfig(DefaultConfig):
 
-    def __init__(self, redis_host: str, redis_port: int, redis_db: int = 0, redis_TTL: int = 3600, **kwargs):
+    def __init__(self, redis_host: str, redis_port: int, redis_db: int = 0, **kwargs) -> None:
         super().__init__(**kwargs)
         self.redis_host = redis_host
         self.redis_port = redis_port
         self.redis_db = redis_db
-        self.redis_TTL = redis_TTL
+        self._redis_TTL: Union[int, None] = 3600
+
+    @property
+    def redis_TTL(self) -> Union[int, None]:
+        return self._redis_TTL
+
+    @redis_TTL.setter
+    def redis_TTL(self, TTL_value: Union[int, None]) -> None:
+        self._redis_TTL = TTL_value
