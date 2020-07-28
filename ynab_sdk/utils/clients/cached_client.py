3ff077e7ad1367b8998c252f1494308596aada1c
Tommaso-n <68448080+Tommaso-n@users.noreply.github.com>
2020-07-28 20:26:24 +0200

    feat: Add cache expiration and extra redis configuration

--- ynab_sdk/utils/clients/cached_client.py
+++ ynab_sdk/utils/clients/cached_client.py
@@ -11,11 +11,19 @@ class CachedClient(BaseClient):
 
     def __init__(self, config: CachedConfig):
         super().__init__(config)
-        self.redis = Redis(host=config.redis_host, port=config.redis_port, db=config.redis_db)
+        self.redis = Redis(host=config.redis_host, port=config.redis_port, db=config.redis_db, password=config.redis_pass)
+        self._keys_prefix: str = 'YNAB_ep_'
+
+    def clear_cache(self) -> None:
+        keys_count: int = 0
+        for k in self.redis.scan_iter(''.join([self._keys_prefix, '*'])):
+            self.redis.delete(k)
+            keys_count += 1
+        self.logger.info(f'clear_cache: {keys_count} keys deleted from cache')
 
     def get(self, endpoint: str):
         self.logger.error(f'Endpoint => {endpoint}')
-        cached_data = self.redis.get(endpoint)
+        cached_data = self.redis.get(''.join([self._keys_prefix, endpoint]))
 
         if cached_data:
             self.logger.error('Using cached data')
@@ -23,11 +31,11 @@ class CachedClient(BaseClient):
         else:
             self.logger.error('Cached data not found, searching for new one')
             url = self.config.full_url + endpoint
-            self.logger.debug(f'Sending get at  {url}')
+            self.logger.debug(f'Sending get at {url}')
             response = requests.get(url, headers=self.headers)
             data = response.json()
             if response.status_code == 200:
-                self.redis.set(endpoint, json.dumps(data))
+                self.redis.set(''.join([self._keys_prefix, endpoint]), json.dumps(data), self.config.redis_ttl)
             else:
                 self.logger.error(f'Error when getting {url}')
                 self.logger.error(data)
