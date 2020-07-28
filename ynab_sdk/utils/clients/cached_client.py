a1c195e547843f197c143c04bf9b8d45bc7cd2bd
Tommaso-n <68448080+Tommaso-n@users.noreply.github.com>
2020-07-28 03:00:18 +0200

    Requested fix

--- ynab_sdk/utils/clients/cached_client.py
+++ ynab_sdk/utils/clients/cached_client.py
@@ -2,7 +2,6 @@ import json
 
 import requests
 from redis import Redis
-from typing import Union
 
 from ynab_sdk.utils.clients.base_client import BaseClient
 from ynab_sdk.utils.configurations.cached import CachedConfig
@@ -13,28 +12,14 @@ class CachedClient(BaseClient):
     def __init__(self, config: CachedConfig):
         super().__init__(config)
         self.redis = Redis(host=config.redis_host, port=config.redis_port, db=config.redis_db, password=config.redis_pass)
-        self._redis_ttl: Union[int, None] = 3600
         self._redis_prefix: str = 'YNAB_ep_'
-
-    @property
-    def cache_time_to_live(self) -> Union[int, None]:
-        return self._redis_ttl
-
-    @cache_time_to_live.setter
-    def cache_time_to_live(self, ttl_value: Union[int, None]) -> None:
-        if ttl_value and ttl_value > 0:
-            self.logger.error(f'cache_time_to_live: TTL value set to {ttl_value}')
-            self._redis_ttl = ttl_value
-        else:
-            self.logger.error(f'cache_time_to_live: 0, negative or None TTL value: {ttl_value}, set to None')
-            self._redis_ttl = None
 
     def clear_cache(self) -> None:
         keys_count: int = 0
         for k in self.redis.scan_iter(''.join([self._redis_prefix, '*'])):
             self.redis.delete(k)
             keys_count += 1
-        self.logger.error(f'clear_cache: {keys_count} keys deleted from cache')
+        self.logger.info(f'clear_cache: {keys_count} keys deleted from cache')
 
     def get(self, endpoint: str):
         self.logger.error(f'Endpoint => {endpoint}')
@@ -50,7 +35,7 @@ class CachedClient(BaseClient):
             response = requests.get(url, headers=self.headers)
             data = response.json()
             if response.status_code == 200:
-                self.redis.set(''.join([self._redis_prefix, endpoint]), json.dumps(data), self._redis_ttl)
+                self.redis.set(''.join([self._redis_prefix, endpoint]), json.dumps(data), self.config.redis_ttl)
             else:
                 self.logger.error(f'Error when getting {url}')
                 self.logger.error(data)
--- ynab_sdk/utils/configurations/cached.py
+++ ynab_sdk/utils/configurations/cached.py
@@ -1,11 +1,27 @@
+from typing import Optional, Union
+
 from ynab_sdk.utils.configurations.default import DefaultConfig
 
 
 class CachedConfig(DefaultConfig):
 
-    def __init__(self, redis_host: str, redis_port: int, redis_db: int = 0, redis_pass: str = None, **kwargs) -> None:
+    def __init__(self, redis_host: str, redis_port: int, redis_db: int = 0, redis_pass: Optional[str] = None, **kwargs) -> None:
         super().__init__(**kwargs)
         self.redis_host = redis_host
         self.redis_port = redis_port
         self.redis_db = redis_db
         self.redis_pass = redis_pass
+        self._redis_ttl: Optional[int] = 3600
+
+    @property
+    def redis_ttl(self) -> Union[int, None]:
+        return self._redis_ttl
+
+    @redis_ttl.setter
+    def redis_ttl(self, ttl_value: Optional[int]) -> None:
+        if ttl_value and ttl_value > 0:
+            self.logger.debug(f'redis_ttl: TTL value set to {ttl_value}')
+            self._redis_ttl = ttl_value
+        else:
+            self.logger.debug(f'redis_ttl: 0, negative or None TTL value: {ttl_value}, set to None')
+            self._redis_ttl = None
